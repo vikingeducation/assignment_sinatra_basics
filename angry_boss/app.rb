c4b29501728b5b57d1e4ab3df3898d8092ba7a88
Sia <siakaramalegos@gmail.com>
2015-12-14 10:15:23 -0600

    Adds response from boss

--- angry_boss/app.rb
+++ angry_boss/app.rb
@@ -5,3 +5,9 @@ require 'erb'
 get '/boss' do
   erb :boss_form
 end
+
+post '/boss' do
+  greeting = params[:greeting]
+  response = "WHAT DO YOU MEAN, '#{greeting.upcase}'???? YOU'RE FIRED!!"
+  erb :boss_response, locals: { response: response }
+end
