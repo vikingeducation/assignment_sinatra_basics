049b357ee933af7672af334daf2bca75551af9bf
Jeffrey Gisin <jgisin@gmail.com>
2016-01-20 11:29:06 -0700

    rpsD works

--- app.rb
+++ app.rb
@@ -16,3 +16,48 @@ end
 #locals hash send to ERB to be able to use
 #locals hash sets an ERB variable = whatever (usually a parameter of the user input)
 ##Anytime ERB needs a variable, you must use the locals hash
+
+get '/rps' do
+
+  erb :player_input
+
+end
+
+post '/rps' do
+
+  comp_selection = ["rock", "paper", "scissors", "dynamite"].sample
+  move = params[:move]
+
+  if move == comp_selection
+    victory = "tied"
+  end
+  if move == "rock"
+    if comp_selection == "paper"
+      victory = "lost"
+    else
+      victory = "won!!!"
+    end
+
+  elsif move == "paper"
+    if comp_selection == "rock"
+      victory = "won!!!"
+    else
+      victory = "lost"
+    end
+
+  elsif move == "scissors"
+    if comp_selection == "paper"
+      victory = "won!!"
+    else
+      victory = "lost"
+    end
+  end
+
+  if comp_selection == "dynamite"
+    victory = "you lost, computer blew you up with dynamite!"
+  end
+      
+  erb :outcome, :locals => {:victory => victory}
+
+
+end
