5959b6ff079b293f6956b04c629f34fd64653df9
Alex Lach <alexglach@gmail.com>
2016-07-21 13:45:01 -0400

    finished rps

--- sinatra_rps/app.rb
+++ sinatra_rps/app.rb
@@ -13,11 +13,11 @@ post '/choice' do
   result = compare_choices(user_choice, computer_choice)
 
   if result == 1
-    erb :win, locals: {user: user_choice, computer: computer_choice}
+    erb :win, locals: {user: user_choice, computer: computer_choice, user_image: get_image(user_choice), computer_image: get_image(computer_choice)}
   elsif result == -1
-    erb :lose, locals: {user: user_choice, computer: computer_choice}
+    erb :lose, locals: {user: user_choice, computer: computer_choice, user_image: get_image(user_choice), computer_image: get_image(computer_choice)}
   else
-    erb :tie, locals: {user: user_choice, computer: computer_choice}
+    erb :tie, locals: {user: user_choice, computer: computer_choice, user_image: get_image(user_choice), computer_image: get_image(computer_choice)}
   end
 end 
 
@@ -32,6 +32,15 @@ def compare_choices(user, computer)
   else
     return 0
   end
+end
+
+def get_image(choice)
+  images = {"rock" => "https://i.ytimg.com/vi/cRnE9j9SzMU/maxresdefault.jpg",
+            "paper" => "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTrqJW41HzsoVavrtVbokAiJ_cJdbyZqcBADtJnOctCJso3LRAYRw",
+            "scissors" => "http://38.media.tumblr.com/15216eda2e8af7795786b339b7ebdd26/tumblr_nbyrv9LSCB1rp0vkjo1_500.gif"
+  }
+
+  images[choice]
 end
 
 
