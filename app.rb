78265f1fcf52655fccd02e9481cf6b3b2bdf7b95
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-21 13:23:13 -0400

    Finish RPS assignment

--- app.rb
+++ app.rb
@@ -13,21 +13,29 @@ post '/boss_form' do
   erb :show_response, locals: {response: show_response}
 end
 
+###############################################
+
 get '/rps' do 
-  erb :rps
+  erb :rps, :layout => :rps_layout
 end
 
 post '/rock_paper_scissors' do 
   user_choice = params[:rps]
   answers = {
-    r: 'p',
-    p: 's',
-    s: 'r'
+    rock: 'paper',
+    paper: 'scissor',
+    scissor: 'rock'
   }
   # values defeat keys
-  computer_choice = %w(r p s).sample
+  computer_choice = %w(rock paper scissor).sample
   if answers[user_choice] == computer_choice
-    puts "computer won"
+    response = "Computer won! Comptuter picked #{computer_choice}, you picked #{user_choice}"
+  elsif user_choice == computer_choice
+    response = "It was a tie! You both picked #{user_choice}"
   else
+    response = "YOU WON!Comptuter picked #{computer_choice}, you picked #{user_choice}"
+  end
+
+  erb :rps_result, :layout => :rps_layout, locals: {response: response}
 
 end
