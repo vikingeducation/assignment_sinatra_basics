6439221a70904b1158ffc53f7c8f4bbdb2c38067
cjvirtucio87 <cjv287@gmail.com>
2016-07-21 09:34:36 -0700

    RPS done.

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
+require './modules/helpers'
 require 'sinatra'
+
+
+helpers Helpers
 
 get '/boss' do
   erb :boss, :locals=>{:input=>nil}
@@ -10,7 +14,11 @@ post '/boss' do
 end
 
 get '/rps' do
+  erb :rps, locals: { result: nil }
 end
 
 post '/rps' do
+  input = params[:rps_choices]
+  result = win?(input) unless input.nil?
+  erb :rps, locals: { result: result }
 end
