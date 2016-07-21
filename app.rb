e12bf4b726a501848d364e9538a09bb2770d748f
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-21 12:38:07 -0400

    Finish boss response app

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ get '/boss' do
   erb :boss
 end
 
-post '/boss' do 
+post '/boss_form' do 
+  question = params[:user_input].upcase
+  show_response = "WHAT DO YOU MEAN, #{question}????? YOU'RE FIRED!1!1"
 
+  erb :show_response, locals: {response: show_response}
 end
