0a5965f4958e4689c2a533d4d17038dd0f310a24
Joe Bernardi <joebernardi@hotmail.com>
2016-09-27 19:17:57 -0700

    working rps game

--- rps.rb
+++ rps.rb
@@ -18,9 +18,39 @@ require 'pry-byebug'
 		player_move = params[ :move ]
 		cpu_move = Computer.new.get_move
 
+		result = outcome( player_move, cpu_move )
 
 
-		erb :show_rps, locals: { move: player_move, cpu: cpu_move }
+		erb :show_rps, locals: { move: player_move, cpu: cpu_move, result: result }
+
+	end
+
+
+	def outcome( player_move, cpu_move )
+
+		return "It's a tie" if player_move == cpu_move
+
+		if player_move == 'rock'
+
+			return "You win!" if cpu_move == 'scissors'
+
+			return "CPU wins!" if cpu_move == 'paper'
+
+		elsif player_move == "paper"
+
+			return "You win!" if cpu_move == 'rock'
+
+			return "CPU wins!" if cpu_move == 'scissors'
+
+		elsif player_move == "scissors"
+
+			return "You win!" if cpu_move == 'paper'
+
+			return "CPU wins!" if cpu_move == 'rock'
+
+		end
+
+
 
 	end
 # when players submit, the post will take the value
