ecfa52a069b46e202c3d1f9cd2d58ff9ade1d149
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-16 14:06:29 -0500

    RPS! WOOO!

--- rps/app.rb
+++ rps/app.rb
@@ -20,10 +20,16 @@ post "/play" do
     response.set_cookie("num_of_wins", 0)
   else
     num_of_wins = request.cookies['num_of_wins']
-    response.set_cookie('num_of_wins', num_of_wins + 1) if outcome == "VICTORY!"
+    response.set_cookie('num_of_wins', num_of_wins.to_i + 1) if outcome == "VICTORY!"
   end
 
-  erb :results, locals: {your_move: player_move, bot_move: bot_move, outcome: outcome}
+  win_display = request.cookies['num_of_wins'].to_i + 1
+  win_display = request.cookies['num_of_wins'] unless outcome == "VICTORY!"
+  win_display = 0 unless win_display
+  win_display = 1 if outcome == "VICTORY!" && win_display == 0
+
+  erb :results, locals: {your_move: player_move, bot_move: bot_move, 
+                        outcome: outcome, wins: win_display}
 
 end
 
@@ -32,3 +38,8 @@ end
 ####post request with input
 ####return win/loss/draw
 ####link back to root 
+
+#win display = 0 if there are no cookies and no victory
+#win display = 1 if there is victory and no cookie
+#win diplay -= cookie if there is no victory
+#wind display = cookie + 1 if there is victory 
