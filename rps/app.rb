ff9450ddb5b8689e34e0d6f3a37ca4f9abacad5a
Alexa <alexa.anderson@outlook.com>
2016-11-16 11:52:23 -0600

    Finish rps

--- rps/app.rb
+++ rps/app.rb
@@ -3,8 +3,22 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 helpers do
-  def method_name
+  def ai_move_selector
+    ['r', 'p', 's'].sample
+  end
 
+  def compare(player_move, ai_move)
+    winners = { "r" => "s",
+                "s" => "p",
+                "p" => "r" }
+    if winners[player_move] == ai_move
+      winner = "Player" 
+    elsif winners[ai_move] == player_move
+      winner = "Computer" 
+    else
+      winner = "No one"
+    end
+    winner
   end
 end
 
@@ -14,5 +28,7 @@ end
 
 post '/results' do
   move = params[:move]
-  erb :results, locals: { move: move }
+  ai_move = ai_move_selector
+  winner = compare(move, ai_move)
+  erb :results, locals: { move: move, ai_move: ai_move, winner: winner }
 end
