dc3e6f69ad60055a7a6a0ebac7608abead29728a
James Harris <computersherpa@gmail.com>
2016-11-16 11:38:51 -0600

    Start work on RPS

--- app.rb
+++ app.rb
@@ -2,14 +2,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/boss' do
-  erb :boss_form
+get '/' do
+  erb :game
 end
 
-post '/boss' do
-  message = params[:message]
+post '/result' do
   p params
-  # Send that data to our view template explicitly
-  # as the `name` variable
-  erb :boss_response, :locals => { :message => message }
 end
