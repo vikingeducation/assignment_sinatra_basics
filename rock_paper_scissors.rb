8e217ec01bf0a28cb0067ca78235f819c73af59e
Dexter Ford <dexter.ford.77@gmail.com>
2016-11-16 12:19:45 -0600

    Working on win conditions for rps

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -2,7 +2,34 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry-byebug' 
 
-get '/' do 
-  
+helpers do
+
+  CHOICES = [:rock, :paper, :scissors]
+  BEATS = {
+    :paper => :scissors,
+    :rock => :paper,
+    :scissors => :rock
+  }
+
+  def computer_choice
+    CHOICES.sample
+  end
+
+  def win_check(user_input, computer_input)
+    user_input = user_input.to_sym
+    case user_input
+    when :rock
+      return "Win" if computer_input = BEATS[:rock]
+    end
+  end
+
 end
 
+get '/' do
+  erb :rps_home
+end
+
+post '/rps' do
+  user_choice = params[:rps]
+  erb :rps_results, locals: {user_choice: user_choice}
+end
