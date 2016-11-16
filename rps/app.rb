d51bcb498b8a8c34eb75d020b01209f3caa80eee
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-16 13:10:14 -0600

    completed rps

--- rps/app.rb
+++ rps/app.rb
@@ -16,17 +16,7 @@ post "/play" do
 
   outcome = determine_outcome(player_move, bot_move)
 
-  unless request.cookies['num_of_wins']
-    response.set_cookie("num_of_wins", 0)
-  else
-    num_of_wins = request.cookies['num_of_wins']
-    response.set_cookie('num_of_wins', num_of_wins.to_i + 1) if outcome == "VICTORY!"
-  end
-
-  win_display = request.cookies['num_of_wins'].to_i + 1
-  win_display = request.cookies['num_of_wins'] unless outcome == "VICTORY!"
-  win_display = 0 unless win_display
-  win_display = 1 if outcome == "VICTORY!" && win_display == 0
+  win_display = cookie_handler(outcome)
 
   erb :results, locals: {your_move: player_move, bot_move: bot_move, 
                         outcome: outcome, wins: win_display}
--- rps/rps_helper.rb
+++ rps/rps_helper.rb
@@ -18,4 +18,21 @@ module GameLogic
 
   end
 
+  def cookie_handler(outcome)
+    unless request.cookies['num_of_wins']
+      response.set_cookie("num_of_wins", 0)
+    else
+      num_of_wins = request.cookies['num_of_wins']
+      response.set_cookie('num_of_wins', num_of_wins.to_i + 1) if outcome == "VICTORY!"
+    end
+
+    win_display = request.cookies['num_of_wins'].to_i + 1
+    win_display = request.cookies['num_of_wins'] unless outcome == "VICTORY!"
+    win_display = 0 unless win_display
+    win_display = 1 if outcome == "VICTORY!" && win_display == 0
+
+    win_display
+
+  end
+
 end
