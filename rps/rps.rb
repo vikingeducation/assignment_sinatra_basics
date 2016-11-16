34869feba780bf2c5e73a052d7539fdeb81277bb
Jake Tingley <jaket32@gmail.com>
2016-11-16 12:53:37 -0500

    finished rps and warmup

--- rps/rps.rb
+++ rps/rps.rb
@@ -4,26 +4,41 @@ require 'sinatra'
 require 'erb'
 require 'sinatra/reloader'
 
-helpers do 
+helpers do
   def random_choice
     ['rock', 'paper', 'scissors'].sample
   end
 
-def determine_outcome
-  #
+def determine_outcome(choice1, choice2)
+  winners = {"rock" => "scissors",
+             "scissors" => "paper",
+             "paper" => "rock"}
+
+  if choice1 == choice2
+    "Tie"
+  elsif winners[choice1] == choice2
+    "You Win!!!"
+  else
+    "I Win!!!"
+  end
 end
 
 end
 
-get '/' do 
+get '/' do
   erb :index
 
 end
 
 
 
-post '/results' do 
+post '/results' do
   user_choice = params[:choice]
- 
-  erb :results, locals:{ outcome: outcome }
+
+  computer_choice = random_choice
+  outcome = determine_outcome(user_choice, computer_choice)
+
+  erb :results, locals:{ outcome: outcome,
+                         user_choice: user_choice,
+                         computer_choice: computer_choice}
 end
