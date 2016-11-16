43ccbad15aba327c0ac95fb9c9191fbc615cb5e3
Pramod Jacob <psjacob68@gmail.com>
2016-11-16 13:52:41 -0500

    completed rock paper scissors

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -2,25 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry-byebug' 
 
+enable :sessions
+
 helpers do
 
   CHOICES = [:rock, :paper, :scissors]
+
   BEATS = {
     :paper => :scissors,
     :rock => :paper,
     :scissors => :rock
   }
 
-  def computer_choice
+  def get_computer_choice
     CHOICES.sample
   end
 
   def win_check(user_input, computer_input)
     user_input = user_input.to_sym
-    case user_input
-    when :rock
-      return "Win" if computer_input = BEATS[:rock]
-    end
+    return "You win" if user_input == BEATS[computer_input] 
+    return "You lose" if computer_input == BEATS[user_input]
+    "Tie" 
   end
 
 end
@@ -29,7 +31,27 @@ get '/' do
   erb :rps_home
 end
 
+# get '/response' do 
+#   erb :rps_results, locals: {
+#     user_choice: session[:user_choice], 
+#     computer_choice: session[:computer_choice],
+#     result: session[:result]
+#   }
+# end
+
 post '/rps' do
   user_choice = params[:rps]
-  erb :rps_results, locals: {user_choice: user_choice}
+  computer_choice = get_computer_choice
+  result = win_check(user_choice, computer_choice)
+  erb :rps_results, locals: { 
+    user_choice: user_choice.to_s,
+    computer_choice: computer_choice.to_s, 
+    result: result 
+  }
+  # session[:user_choice] = user_choice.to_s,
+  # session[:computer_choice] = computer_choice.to_s,
+  # session[:result] = result
+  # redirect to('/response')
 end
+
+# ASK IN SCRUM
