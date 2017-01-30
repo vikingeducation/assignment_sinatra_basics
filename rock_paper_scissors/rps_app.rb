91b11ee9626353f408d7f197de45e3c5fe9b8857
Visiona <darbis@gmail.com>
2017-01-30 00:19:58 +0000

    Created simple Rock Scissors Web App

--- rock_paper_scissors/rps_app.rb
+++ rock_paper_scissors/rps_app.rb
@@ -2,14 +2,15 @@
 require 'sinatra'
 require 'pry-byebug'
 
-get '/rps_display' do
+
+get '/' do
   erb :rps_display
 end
 
 post '/rps_choose' do
 
   # binding.pry
-  get_decision = params[:decision]
+  get_decision = params[:h_decision]
   erb :rps_result, locals: {h_decision: get_decision }
 
 end
