94bd7a27c100d24760a86f26a8a079c6ab499808
Roy Chen <roychenlei@gmail.com>
2017-05-10 10:18:49 +0800

    added route for form submission

--- rock_paper_scissors/app.rb
+++ rock_paper_scissors/app.rb
@@ -11,5 +11,19 @@ get "/rps" do
 end
 
 post "/rps" do
-  erb :result
+  player_move = params[:move]
+  computer_move = ["rock", "paper", "scissors"].sample
+  winner = nil
+
+  if player_move == computer_move
+    winner = :tie
+  else
+    if (player_move == "rock" && computer_move == "scissors") || (player_move == "scissors" && computer_move == "paper") || (player_move == "paper" && computer_move == "rock")
+      winner = :player
+    else
+      winner = :computer
+    end
+  end
+
+  erb :result, :locals => { :player_move => player_move, :computer_move => computer_move, :winner => winner }
 end
