1506bc328d713ff174e8d4a35ef3d4b0adc99d85
anne <richardson.ae@gmail.com>
2017-10-25 20:53:13 -0500

    Add weapon choice logic to router

--- rock_paper_scissors/app.rb
+++ rock_paper_scissors/app.rb
@@ -3,11 +3,16 @@ require 'sinatra/reloader' if development?
 require 'erb'
 require 'pry'
 
+require './helpers/weapons_helper'
+helpers WeaponsHelper
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  erb :verdict
+  human = params[:human_choice]
+  computer = computer_choice
+  winner = determine_winner(human, computer)
+  erb :verdict, locals: { human: human, computer: computer,winner: winner }
 end
